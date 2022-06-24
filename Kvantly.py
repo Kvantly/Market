db1a4ce905977f6ae8e8bb203048e1a1d413eac9
Kvantly <98684079+Kvantly@users.noreply.github.com>
2022-06-24 14:20:45 +0200

    Update Kvantly.py

--- Kvantly.py
+++ Kvantly.py
@@ -73,7 +73,7 @@ st.area_chart(indy[f'{option}'], width=0, height=0, use_container_width=True)
 data = yf.download(option,start= start_date,end= end_date, progress=False)
 data['SMA Fast']   = data['Adj Close'].rolling(Fast_sma).mean()
 data['SMA Slow']   = data['Adj Close'].rolling(Slow_sma).mean()
-data['Sentiment']  = (data['SMA Slow'] - data[['Adj Close'])/ data[['Adj Close']
+data['Sentiment']  = (data['SMA Slow'] - data['Adj Close'])/ data[['Adj Close']
 
 
 
